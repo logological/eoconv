cc9a5afdc8f58465225ed5be5dcccb68436518ed
Tristan Miller <psychonaut@nothingisreal.com>
2004-09-10 20:59:53 +0000

    Added sample documentation

--- eoconv.pl
+++ eoconv.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# $Id: eoconv.pl,v 1.3 2004-09-10 20:52:27 psy Exp $
+# $Id: eoconv.pl,v 1.4 2004-09-10 20:59:53 psy Exp $
 
 # Copyright (C) 2004 Tristan Miller <psychonaut@nothingisreal.com>
 #
@@ -18,6 +18,16 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 # 02111-1307, USA.
+
+use Getopt::Long;
+use Pod::Usage;
+
+my $man = 0;
+my $help = 0;
+
+GetOptions('help|?' => \$help, man => \$man) or pod2usage(2);
+pod2usage(1) if $help;
+pod2usage(-exitstatus => 0, -verbose => 2) if $man;
 
 # To do:
 # - First, convert string to Unicode
@@ -79,3 +89,38 @@ foreach $line (<>) {
 
   print $line;
 }
+
+__END__
+
+=head1 NAME
+
+sample - Using Getopt::Long and Pod::Usage
+
+=head1 SYNOPSIS
+
+sample [options] [file ...]
+
+ Options:
+   -helpbrief help message
+   -man full documentation
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<-help>
+
+Print a brief help message and exits.
+
+=item B<-man>
+
+Prints the manual page and exits.
+
+=back
+
+=head1 DESCRIPTION
+
+B<This program> will read the given input file(s) and do someting
+useful with the contents thereof.
+
+=cut
