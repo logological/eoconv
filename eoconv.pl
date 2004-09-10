b61831924aef711704b9f71a8289b0b84b35437e
Tristan Miller <psychonaut@nothingisreal.com>
2004-09-10 22:39:11 +0000

    Added encoding hash

--- eoconv.pl
+++ eoconv.pl
@@ -1,5 +1,13 @@
 #!/usr/bin/perl
 
+# To do:
+# - First, convert string to Unicode
+# - Then, use the Encode module to encode to whatever
+#   encoding is desired (utf-8, utf-7, utf-16, etc.)
+#   e.g. 
+#      use Encode 'from_to';
+#      from_to($data, "iso-8859-3", "utf-8");
+
 # Copyright (C) 2004 Tristan Miller <psychonaut@nothingisreal.com>
 #
 # This program is free software; you can redistribute it and/or modify
@@ -23,20 +31,23 @@ use Pod::Usage;
 my $man = 0;
 my $help = 0;
 my $version = 0;
-my $from = "";
-my $to = "";
-my $ver='$Id: eoconv.pl,v 1.8 2004-09-10 22:28:54 psy Exp $';
+my $from = 0;
+my $to = 0;
+my $ver='$Id: eoconv.pl,v 1.9 2004-09-10 22:39:11 psy Exp $';
 
 GetOptions('help|?' => \$help,
 	   man => \$man,
 	   version => \$version,
 	   'from=s' => \$from,
 	   'to=s' => \$to,
-	  ) or pod2usage(2);
+	  ) or pod2usage(1);
 
 # Display help/man page
-pod2usage(1) if $help;
+pod2usage(0) if $help;
 pod2usage(-exitstatus => 0, -verbose => 2) if $man;
+
+# Incorrect invocation
+pod2usage(-exitstatus => 1, -verbose => 0) if (!($from && $to));
 
 # Display version information
 if ($version) {
@@ -52,55 +63,58 @@ EOF
   exit 0;
 }
 
-# To do:
-# - First, convert string to Unicode
-# - Then, use the Encode module to encode to whatever
-#   encoding is desired (utf-8, utf-7, utf-16, etc.)
-#   e.g. 
-#      use Encode 'from_to';
-#      from_to($data, "iso-8859-3", "utf-8");
+my @enc_post_x = ("cx", "gx", "hx",
+		  "jx", "sx", "ux",
+		  "Cx", "Gx", "Hx",
+		  "Jx", "Sx", "Ux");
 
-@enc_post_x = ("cx", "gx", "hx",
-	       "jx", "sx", "ux",
-	       "Cx", "Gx", "Hx",
-	       "Jx", "Sx", "Ux");
+my @enc_post_h = ("ch", "gh", "hh",
+		  "jh", "sh", "uh",
+		  "Ch", "Gh", "Hh",
+		  "Jh", "Sh", "Uh");
 
-@enc_post_h = ("ch", "gh", "hh",
-	       "jh", "sh", "uh",
-	       "Ch", "Gh", "Hh",
-	       "Jh", "Sh", "Uh");
+my @enc_post_caret = ("c^", "g^", "h^",
+		      "j^", "s^", "u^",
+		      "C^", "G^", "H^",
+		      "J^", "S^", "U^");
 
-@enc_post_caret = ("c^", "g^", "h^",
-		   "j^", "s^", "u^",
-		   "C^", "G^", "H^",
-		   "J^", "S^", "U^");
+my @enc_pre_caret = ("^c", "^g", "^h",
+		     "^j", "^s", "^u",	
+		     "^C", "^G", "^H",
+		     "^J", "^S", "^U");
 
-@enc_pre_caret = ("^c", "^g", "^h",
-		  "^j", "^s", "^u",	
-		  "^C", "^G", "^H",
-		  "^J", "^S", "^U");
+my @enc_html_hex = ("&#x109;", "&#x11d;", "&#x125;",
+		    "&#x135;", "&#x15d;", "&#x16d;",
+		    "&#x108;", "&#x11c;", "&#x124;",
+		    "&#x134;", "&#x15c;", "&#x16c;");
 
-@enc_html_hex = ("&#x109;", "&#x11d;", "&#x125;",
-		 "&#x135;", "&#x15d;", "&#x16d;",
-		 "&#x108;", "&#x11c;", "&#x124;",
-		 "&#x134;", "&#x15c;", "&#x16c;");
+my @enc_html_dec = ("&#265;", "&#285;", "&#293;",
+		    "&#309;", "&#349;", "&#365;",
+		    "&#264;", "&#284;", "&#292;",
+		    "&#308;", "&#348;", "&#364;");
 
-@enc_html_dec = ("&#265;", "&#285;", "&#293;",
-		 "&#309;", "&#349;", "&#365;",
-		 "&#264;", "&#284;", "&#292;",
-		 "&#308;", "&#348;", "&#364;");
+my @enc_iso_8859_3 = ("\xe6", "\xf8", "\xb6",
+		      "\xbc", "\xfe", "\xfd",
+		      "\xc6", "\xd8", "\xa6",
+		      "\xac", "\xde", "\xdd");
 
-@enc_iso_8859_3 = ("\xe6", "\xf8", "\xb6",
-		   "\xbc", "\xfe", "\xfd",
-		   "\xc6", "\xd8", "\xa6",
-		   "\xac", "\xde", "\xdd");
+my @enc_utf_8 = ("\x{0109}", "\x{011d}", "\x{0125}",
+		 "\x{0135}", "\x{015d}", "\x{016d}",
+		 "\x{0108}", "\x{011c}", "\x{0124}",
+		 "\x{0134}", "\x{015c}", "\x{016c}");
 
-@enc_utf_8 = ("\x{0109}", "\x{011d}", "\x{0125}",
-	      "\x{0135}", "\x{015d}", "\x{016d}",
-	      "\x{0108}", "\x{011c}", "\x{0124}",
-	      "\x{0134}", "\x{015c}", "\x{016c}");
+my %encodings = (
+		 post_x     => \@enc_post_x,
+		 post_h     => \@enc_post_h,
+		 post_caret => \@enc_post_caret,
+		 pre_caret  => \@enc_pre_caret,
+		 html_hex   => \@enc_html_hex,
+		 html_dec   => \@enc_html_dec,
+		 iso_8859_3 => \@enc_iso_8859_3,
+		 utf_8      => \@enc_utf_8
+		);
 
-$from = \@enc_pre_x;
+$from = \@enc_post_x;
 $to   = \@enc_iso_8859_3;
 
 foreach $line (<>) {
